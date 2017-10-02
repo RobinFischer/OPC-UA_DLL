4b651abfcbd38448eb8479ddf7f4f601a6faebee
RobinFischer <robin.fischer@stud.h-da.de>
2017-10-02 20:25:28 +0200

    Add files via upload

---
+++ Dumy/OpcUa_Hololens_client/OpcUa_Hololens_client/OpcUa_Hololens_client/HoloClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpcUa_Hololens_client
+{
+    public class HoloClient
+    {
+        public string conexion_Error;
+        public string output;
+
+        public static string reworkCode_value;
+        public static bool reworkCode_status;
+        public static string reworkDone_value;
+        public static bool reworkDone_status;
+        public static string reworkStart_value;
+        public static bool reworkStart_status;
+
+        public bool setEndpoint(string endpointURL)
+        {
+            return false;
+        }
+
+        public bool connect(string username, string password, string reworkCode_NodeId, string reworkDone_NodeId, string reworkSinpos_NodeId, int publishInterval)
+        {
+            return false;
+        }
+
+        public bool disconnect()
+        { 
+            return false;
+        }
+
+        public bool write(string nodeId, string valueToWrite)
+        {
+            return false;
+        }
+
+        public string read(string nodeId)
+        {
+            return null;
+        }
+
+
+    }
+}
